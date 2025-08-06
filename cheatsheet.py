99a78ad69e31f8f8a2c9d4c4782dfbfdc4bcd307
gitwebz <abdulrehmanwebz@gmail.com>
2025-08-07 00:36:24 +0500

    add api key

--- cheatsheet.py
+++ cheatsheet.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "Enter your API Key here"
+API_KEY = "AIzaSyCAApgwcHLDCzTA0MJkw7MRFU6bDaJ3Wd4"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
